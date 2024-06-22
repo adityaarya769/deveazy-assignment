8080d5683a585fe3de13e511f7c5f3db2a3e0b8f
ADITYA <adityaarya1614@gmail.com>
2024-06-23 01:42:10 +0530

    changes for mongodb function

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI)  
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('MongoDB connection error:', err));;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/files', fileRoutes);
