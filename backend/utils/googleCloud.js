54b2421fbb93cac261045f9e0484bbdc9a5b0e2c
ADITYA <adityaarya1614@gmail.com>
2024-06-23 01:27:50 +0530

    some changes in secret key and its respective variables

--- backend/utils/googleCloud.js
+++ backend/utils/googleCloud.js
@@ -1,11 +1,12 @@
 const { Storage } = require('@google-cloud/storage');
 const path = require('path');
-
+const dotenv = require('dotenv');
+dotenv.config();
 const storage = new Storage({
   keyFilename: path.join(__dirname, '../google-cloud-key.json'),
-  projectId: "scrapi-development",
+  projectId: process.env.GCLOUD_PROJECT_ID,
 });
 
-const bucket = storage.bucket("deveazy-assignment");
+const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
 
 module.exports = bucket;
